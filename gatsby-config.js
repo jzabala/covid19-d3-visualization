7835577f175f3e5806b42b55fb6a9b174f0ceb0f
Johnny Zabala <jzabala.s@gmail.com>
2020-04-07 12:20:11 -0400

    disable offline plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,6 +40,8 @@ module.exports = {
         purgeOnly: [`src/css/style.css`],
       },
     },
-    `gatsby-plugin-offline`,
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
   ],
 }
